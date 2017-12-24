96e22f879210fa5601673938941109921aaab23b
hfkhku <yamada0432@gmail.com>
2017-12-24 16:10:27 +0900

    新規作成

---
+++ MStopwatch/Models/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace MStopwatch.Models
+{
+    public static class Constants
+    {
+        public static string TimeSpanFormat { get; } = @"hh\:mm\:ss\""fff";
+
+        public static string TimeSpanFormatNoMillsecond { get; } = @"hh\:mm\:ss\""";
+
+        public static string DateTimeFormat { get; } = "yyyy/MM/dd HH:mm:ss";
+    }
+}
